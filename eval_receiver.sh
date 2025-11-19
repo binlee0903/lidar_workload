c5d8e90c7551db9c9cf5317e2976289827d89190
binlee0903 <egb3543@gmail.com>
2025-11-19 17:57:23 +0900

    add shell scripts

---
+++ eval_receiver.sh
@@ -0,0 +1,25 @@
+echo "START EXPERIMENT"
+
+source ./velodyne/install/setup.bash
+ros2 run velodyne_driver velodyne_driver_node \
+  --ros-args \
+  -p model:=VLP16 \
+  -p frame_id:=velodyne \
+  -p device_ip:="" \
+  -p port:=2368 &
+VELODYNE_PID=$!
+
+sudo cyclictest -q --smp -p 90 -i 1000 -m > cyclictest_results.txt
+CYCLIC_PID=$!
+
+sleep 2
+
+sudo bash -c "source lidar_env/bin/activate && source /opt/ros/kilted/setup.bash && python receiver.py"
+
+sudo kill -2 $CYCLIC_PID
+
+wait $CYCLIC_PID
+
+sudo kill -9 $VELODYNE_PID
+
+echo "EXCERIMENT END"
---
+++ setting.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install python3-venv
+python3 -m venv lidar_env --system-site-packages
+source ./lidar_env/bin/activate
